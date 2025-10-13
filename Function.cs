c6ae353943512ec9f04f3e3066b2bd6217841411
ben.murphy <ben.murphy@AL-E305-S-06>
2025-10-13 11:24:12 -0400

    Added to-do list to Function

---
+++ Function.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZooPal
+{
+    internal class Function
+    {
+
+        //display animals
+        //display habitats
+        //feed all animals
+        //perform health checks
+        //add new animal
+        //Generate Daily Report
+        //Demonstrate Polymorphism
+        //Emergency Evactuation Drill
+        //Exit
+    }
+}
