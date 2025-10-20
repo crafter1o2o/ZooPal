7431c8a59fb9e41ac2d9979a1ef2da417a9d1174
crafter1o2o <philip.bernsdorff@gmail.com>
2025-10-20 09:12:27 -0400

    Did ALOT
    Added Classes
    UI
    Goodbye
    
    Did some simple Logic, and a shit ton of other stuff
    
    Edit: Now I am just adding comments and shiz like that lol

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,20 +10,103 @@ namespace ZooPal
 {
     internal class Program
     {
-
         static void Main(string[] args)
         {
-            habitat savannahHabitat = new Habitat.habitat("Savannah", 10, 30.0, 5000.0, true);
+
+
+            //List for the ALL animals
+            List<String> listAnimals = new List<String>();
+
+
+
+            //Create the Animal of choice
             animal lion1 = new animal("Leo", "lion", 5, 190.5, true, DateTime.Now.AddHours(-3), "Savannah");
 
 
-            Console.WriteLine(savannahHabitat.displayInfo());
-            //Currently, no animals being made are being adding animals to habitat, fix that somehow, figuture it out soon.
-            Console.WriteLine("\n");
 
-            Console.WriteLine(lion1.displayInfo());
-            
+            //Add the animal to the list
+            ///I Unfortunately don't know how to automatically add animals to habitats yet, so for now it's manual. -PCB
+            listAnimals.Add(lion1.displayInfo());
 
+
+
+            //Create Habitats
+            ///I will be figuring out how to add animals to habitats automatically later. For now, it's manual. -PCB
+            habitat savannahHabitat = new Habitat.habitat("Savannah", 10, 30.0, 5000.0, true);
+
+            //To Stop The Program, Make this bish false
+            bool isRunning = true;
+
+
+            //Take user input
+            String userInput;
+            string userInput1;
+
+
+
+            if (isRunning == true)
+            {
+                //Makes the menu display
+                UI.DisplayMenu();
+
+                Console.Write("Please select an option (1-8): ");
+                userInput = Console.ReadLine();
+                userInput1 = userInput;
+                ///YES I KNOW IT'S REPEATIVE! BUT THIS IS WORKING!!! -PCB
+
+                Console.Clear();
+                switch (userInput1)
+                {
+                    case "1":
+                        for (int i = 0; i < 1; i++)
+                        {
+                            Console.WriteLine(listAnimals[i]);
+                        }
+
+                        //Display Animals
+                        userInput = Console.ReadLine();
+                        Console.Clear();
+                        break;
+                    case "2":
+                        //Display Habitats
+                        Console.WriteLine(savannahHabitat.displayInfo());
+                        //Currently, no animals being made are being adding animals to habitat, fix that somehow, figuture it out soon.
+
+                        break;
+                    case "3":
+                        //Feed All Animals
+                        break;
+                    case "4":
+                        //Perform Health Checks
+                        break;
+                    case "5":
+                        //Add New Animal
+                        break;
+                    case "6":
+                        //Generate Daily Report
+                        break;
+                    case "7":
+                        //Emergency Evacuation Drill
+                        break;
+                    case "8":
+                        //Exit
+                        
+                        
+                        Goodbye.displayGoodbye();
+                        isRunning == false;
+                        Enviroment.Exit(0);
+
+                        break;
+                    default:
+                        Console.WriteLine("Invalid input, please try again.");
+                        break;
+                }
+            }
+        }
+    }
+}
+
+          
             //display animals
            
            
@@ -40,6 +124,4 @@ namespace ZooPal
             //Demonstrate Polymorphism
             //Emergency Evactuation Drill
             //Exit
-        }
-    }
-}
+
