b50f155dd08a791854db0e791db87b6f54d22ebe
crafter1o2o <philip.bernsdorff@gmail.com>
2025-10-20 09:12:27 -0400

    Did ALOT
    Added Classes
    UI
    Goodbye
    
    Did some simple Logic, and a shit ton of other stuff

---
+++ Goodbye.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZooPal
+{
+    internal class Goodbye
+    {
+        public static void displayGoodbye()
+        {
+            Console.WriteLine("Thank you for using Zoo Pal System! Goodbye!");
+        }
+    }
+}
+
---
+++ UI.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZooPal
+{
+    internal class UI
+    {
+        public static void DisplayMenu()
+        {
+            Console.WriteLine("-------Zoo Pal Zoo-------");
+            Console.WriteLine("1. Display Animals");
+            Console.WriteLine("2. Display Habitats");
+            Console.WriteLine("3. Feed All Animals");
+            Console.WriteLine("4. Perform Health Checks");
+            Console.WriteLine("5. Add New Animal");
+            Console.WriteLine("6. Generate Daily Report");
+            Console.WriteLine("7. Emergency Evacuation Drill");
+            Console.WriteLine("8. Exit");
+            Console.WriteLine("-------------------------");
+
+
+        }
+    }
+}
