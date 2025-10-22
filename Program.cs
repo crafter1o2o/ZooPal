c5a0ee0bb5069a38c644cda02c9e7dde7e132392
Ben Murphy <DeepFreeze172@gmail.com>
2025-10-22 11:49:16 -0400

    Change Animals, Preserve, and Program

--- Program.cs
+++ Program.cs
@@ -16,8 +16,98 @@ namespace ZooPal
 
         static void Main(string[] args)
         {
+            //List for the ALL animals
+            List<String> listAnimals = new List<String>();
 
+
+
+            //Create the Animal of choice
+            animal lion1 = new animal("Leo", "lion", 5, 190.5, true, DateTime.Now.AddHours(-3), "Savannah");
+
+
+
+            //Add the animal to the list
+            ///I Unfortunately don't know how to automatically add animals to habitats yet, so for now it's manual. -PCB
+            listAnimals.Add(lion1.displayInfo());
+
+
+
+            //Create Habitats
+            ///I will be figuring out how to add animals to habitats automatically later. For now, it's manual. -PCB
+            habitat savannahHabitat = new Habitat.habitat("Savannah", 10, 30.0, 5000.0, true);
+
+            //To Stop The Program, Make this bish false
+            bool isRunning = true;
+
+
+            //Take user input
+            String userInput;
+            string userInput1;
+
+
+
+            while (isRunning == true)
+            {
+                //Makes the menu display
+                UI.DisplayMenu();
+
+                Console.Write("Please select an option (1-9): ");
+                userInput = Console.ReadLine();
+                userInput1 = userInput;
+                ///YES I KNOW IT'S REPEATIVE! BUT THIS IS WORKING!!! -PCB
+
+                Console.Clear();
+                switch (userInput1)
+                {
+                    case "1":
+                        for (int i = 0; i < 1; i++)
+                        {
+                            Console.WriteLine(listAnimals[i]);
+                        }
+
+                        //Display Animals
+                        userInput = Console.ReadLine();
+                        Console.Clear();
+                        break;
+                    case "2":
+                        //Display Habitats
+                        Console.WriteLine(savannahHabitat.displayInfo());
+                        //Currently, no animals being made are being adding animals to habitat, fix that somehow, figuture it out soon.
+                        break;
+                    case "3":
+                        //Feed All Animals
+                        break;
+                    case "4":
+                        //Perform Health Checks
+                        break;
+                    case "5":
+                        //Add New Animal
+                        break;
+                    case "6":
+                        //Generate Daily Report
+                        break;
+                    case "7":
+                        //Emergency Evacuation Drill
+                        break;
+                    case "8":
+
+                        break;
+                    case "9":
+                        ///GOD DAMNIT! This stupid POS won't read this for some reason.
+                        ///10-22 FIXED IT!!! WOOOOOO!!! Also I did just change the case for this cause I finally realized I didn't make the Main Menu Properly-PCB
+                        //Exit
+                        Console.Clear();
+                        Console.WriteLine("Exiting the program. Goodbye!");
+                        isRunning = false;
+                        break;
+                    default:
+                        Console.WriteLine("Invalid input, please try again.");
+                        break;
+
+                }
+            }
         }
+
 
         public void InitializePreserve()
         {
@@ -28,7 +118,7 @@ namespace ZooPal
         {
 
         }
-        
+
         public void DisplayMenu()
         {
 
@@ -48,5 +138,6 @@ namespace ZooPal
         {
 
         }
+
     }
 }
