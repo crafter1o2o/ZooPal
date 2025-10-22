4d56cf677b4af9b3dd8ecdbec5bda658d6526ae3
Mark Pepling <budderninja9000@gmail.com>
2025-10-22 11:58:46 -0400

    Added Savanna, aviary, Aquarium

---
+++ Aviary.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZooPal
+{
+    internal class Aviary 
+    {
+        public class Aviary : animal
+        {
+            public double FightHeight;
+            public int Perchcount;
+            public bool HasNettingRoof;
+
+
+
+
+
+            private class AddAnimal
+            {
+
+
+
+            }
+
+            private void DisplayInfo()
+            {
+
+            }
+
+
+            private void PerformMatanence()
+            {
+
+            }
+
+
+
+        }
+        }
+    }
