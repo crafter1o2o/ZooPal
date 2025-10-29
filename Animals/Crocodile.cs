1e80674974969246af1cfc06f3af59988493e8b1
mark.pepling <mark.pepling@AL-E305-S-08>
2025-10-29 11:39:05 -0400

    Added Penguin and Crocodile

---
+++ Animals/Crocodile.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZooPal.Animals
+{
+    internal class Crocodile
+    {
+        double Biteforce;
+        int TeethCount;
+    }
+}
