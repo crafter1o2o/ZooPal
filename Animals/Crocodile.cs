26bc134ea0f8f935ca97fc9c889e9737bf2cd73e
Ben Murphy <DeepFreeze172@gmail.com>
2025-11-03 13:15:09 -0500

    Added Crocodile and Penguin

---
+++ Animals/Crocodile.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+/// <summary>
+/// Summary description for Class1
+/// </summary>
+public class Crocodile
+{
+	public Crocodile()
+	{
+		public double BiteForce;
+		public int TeethCount;
+
+	public Crocodile(string name, int age, double weight, double biteForce)
+	{
+
+	}
+
+	public string MakeSound()
+	{
+
+	}
+
+	public string GetDietType()
+	{
+		
+	}
+
+	public double GetFoodAmount()
+	{
+
+	}
+	public void Feed()
+	{
+
+	}
+
+	public void DisplayInfo()
+	{
+
+	}
+
+	}
+}
